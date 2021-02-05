6bfb3b54975d2d07abd87537ed0d4c8c61c48d32
Go_g4 <goscha-grishenkov@yandex.ru>
2021-02-05 22:30:00 +0300

    rotations and shearing

--- structures.py
+++ structures.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import cos, sin
 
 def equal(a: float, b: float, eps: float = 0.001) -> bool:
     return abs(a - b) < eps
@@ -18,7 +19,54 @@ def cross(x: np.array, y: np.array) -> np.array:
 
 
 def point(x: float, y: float, z: float, w: float = 1.) -> np.array:
+    # return np.array([[x, y, z, w]], float).T  # point is a vector column
     return np.array([x, y, z, w], float)
 
 def vector(x: float, y: float, z: float, w: float = 0.) -> np.array:
+    # return np.array([[x, y, z, w]], float).T  # vector is a vector column
     return np.array([x, y, z, w], float)
+
+def translation(x: float, y: float, z: float) -> np.array:
+    tmp = np.identity(4, float)
+    tmp[0][3] = x
+    tmp[1][3] = y
+    tmp[2][3] = z
+    return tmp
+
+def mult(m1: np.array, m2: np.array) -> np.array:
+    return np.matmul(m1, m2)
+
+def scaling(x: float, y: float, z: float) -> np.array:
+    return np.diag([x, y, z, 1.])
+
+
+def rotationX(r: float) -> np.array:
+    tmp = np.diag([1., cos(r), cos(r), 1.])
+    s = sin(r)
+    tmp[2][1] = s
+    tmp[1][2] = -s
+    return tmp
+
+def rotationY(r: float) -> np.array:
+    tmp = np.diag([cos(r), 1., cos(r), 1.])
+    s = sin(r)
+    tmp[0][2] = s
+    tmp[2][0] = -s
+    return tmp
+
+def rotationZ(r: float) -> np.array:
+    tmp = np.diag([cos(r), cos(r), 1., 1.])
+    s = sin(r)
+    tmp[0][1] = -s
+    tmp[1][0] = s
+    return tmp
+
+def shearing(xy: float, xz: float, yx: float, yz: float, zx: float, zy: float) -> np.array:
+    tmp = np.identity(4)
+    tmp[0][1] = xy
+    tmp[0][2] = xz
+    tmp[1][0] = yx
+    tmp[1][2] = yz
+    tmp[2][0] = zx
+    tmp[2][1] = zy
+    return tmp
